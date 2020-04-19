e32ef09ad01d4d769fdb0bcc154577b38a29f3f1
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-04-19 07:10:26 +0200

    regroupemnet des fichiers en dossiers et création fichier config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# To generate a new secret key:
+# import random
+# import string
+# key = "".join([random.choice(string.printable) for _ in range(24)])
+# print(key)
+
+SECRET_KEY = "#d$JCqTTW\nilK\\7m\x0bp#\tj~#H"
+
+FB_APP_ID = 235628367653490
